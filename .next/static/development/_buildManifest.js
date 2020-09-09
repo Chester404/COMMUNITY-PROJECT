4c83e3b4b33e2c1ee7bfc07caedaa187c2ce2863
chester404 <chestermawuli@gmail.com>
2020-09-09 21:24:34 +0000

    profile edit and view update

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/editprofile":["static\u002Fchunks\u002Fpages\u002Feditprofile.js"],"/profile":["static\u002Fchunks\u002Fpages\u002Fprofile.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/signup":["static\u002Fchunks\u002Fpages\u002Fauth\u002Fsignup.js"],"/memberlist":["static\u002Fchunks\u002Fpages\u002Fmemberlist.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
