254cfd033500e73d93d750fa9cb1f778d41d6c04
James Anyimiah Asafuah jr <slerches@gmail.com>
2020-09-14 13:29:52 +0000

    Temporal commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,15 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
-<<<<<<< HEAD
-/******/ 	var hotCurrentHash = "7062663e265131921310";
-=======
-/******/ 	var hotCurrentHash = "57cdccbae788d5825bd0";
->>>>>>> pre-release
-=======
-/******/ 	var hotCurrentHash = "44749ed5a36947285a12";
->>>>>>> pre-release
+/******/ 	var hotCurrentHash = "670c232a97f53cc7b9f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
