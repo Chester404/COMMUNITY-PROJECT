c858ff725ca8ff9344db1a352635963f2f6a8a43
Isaac <isaac@getinnotized.com>
2020-09-22 09:32:38 +0000

    Committing to merge

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/editprofile":["static\u002Fchunks\u002Fpages\u002Feditprofile.js"],"/memberlist":["static\u002Fchunks\u002Fpages\u002Fmemberlist.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/login":["static\u002Fchunks\u002Fpages\u002Fauth\u002Flogin.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
