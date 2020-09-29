065ec20370f294dcdaca4adb0605289a08ad35b4
Isaac <isaac@getinnotized.com>
2020-09-29 12:30:41 +0000

    Merging with view-all-products

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "/_error": ["static\u002Fchunks\u002Fpages\u002F_error.js"],
-  "/userList": ["static\u002Fchunks\u002Fpages\u002FuserList.js"],
-};
-self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/products/addproduct":["static\u002Fchunks\u002Fpages\u002Fproducts\u002Faddproduct.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
