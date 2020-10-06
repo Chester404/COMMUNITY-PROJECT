0d9054835c54588d4f1b9a9c56c97d7612b27a68
Isaac <isaac@getinnotized.com>
2020-10-06 15:12:57 +0000

    Conflict resolved

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/next/dist/pages/_error":["static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/businessprofile":["static\u002Fchunks\u002Fpages\u002Fbusinessprofile.js"],"/editbusinessprofile":["static\u002Fchunks\u002Fpages\u002Feditbusinessprofile.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> de9ff1e73ad9acf5f7dd0de4b886cdd983abf8fc
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/account-settings":["static\u002Fchunks\u002Fpages\u002Fauth\u002Faccount-settings.js"],"/auth/login":["static\u002Fchunks\u002Fpages\u002Fauth\u002Flogin.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
