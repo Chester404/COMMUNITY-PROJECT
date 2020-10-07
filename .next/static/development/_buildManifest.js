8aba55b00254018d1fce2776a9e5cbdd3ed660c7
Isaac <isaac@getinnotized.com>
2020-10-07 04:53:59 +0000

    Navbar fixes

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/account-settings":["static\u002Fchunks\u002Fpages\u002Fauth\u002Faccount-settings.js"],"/auth/login":["static\u002Fchunks\u002Fpages\u002Fauth\u002Flogin.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/memberlist":["static\u002Fchunks\u002Fpages\u002Fmemberlist.js"],"/next/dist/pages/_error":["static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 3ac788b39b98a53683f121b08d6894321855fbc4
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/signup":["static\u002Fchunks\u002Fpages\u002Fauth\u002Fsignup.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
