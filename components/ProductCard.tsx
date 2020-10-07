a32562f162bace926f9b8a53f96755963f36250f
Kwame Charles <68544255+amoah-ansong@users.noreply.github.com>
2020-10-07 11:27:39 +0000

    worked on market views

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
           id="cart-button"
           className="btn btn-info card-shopping"
         >
-          <i className="fe fe-shopping-bag cart-icon"></i>
+          <i className="fe fe-shopping-cart cart-icon"></i>
         </button>
         <img
           src={product.image_1}
@@ -17,63 +17,29 @@ const ProductCard = ({ product }) => {
           alt="..."
           style={{ height: "12.5rem", objectFit: "fill", zIndex: 1 }}
         />
-        <div className="card-body justify-content-between" id="product-card-body">
-          <div className="row" id="product-card__inner-row">
-            <div
-              className="col-md-10"
-              style={{ paddingRight: "2em" }}
-              id="product-card__inner-row-content"
+        <div className="card-body" id="product-card-body">
+          <div className="row  "
+           id="product-card__inner-row-name"
+          >
+               <h4 className="market-product-name">{product.name}</h4>
+          </div>
+
+          <div className="row justify-content-between">
+            <div className=" mt-2"
+             id="product-card__inner-row-price"
             >
-              <div
-                className="font-weight-bold row"
-                id="product-card__inner-row-name"
+              {`GHS ${product.price}`}
+            </div>
+            <div className="">
+            <input
+                id="password"
+                type="number"
+                className="form-control form-rounded cart-quantity"
+                placeholder="Qty:"
+                
               >
-                {product.name}
+              </input>
               </div>
-              <div
-                className="row"
-                id="product-card__inner-row-price"
-              >{`GHS ${product.price}`}</div>
-            </div>
-            <div
-              className="col-md-2 d-flex align-items-center"
-              id="product-card__quantity-div"
-            >
-              <div className="row float-right" id="product-card__quantity-row" >
-                <div className="qty mt-5 btn-group" id="product-card-qty" style={{width:"5em"}}>
-                  <span
-                    id="product-card-qty-minus"
-                    className="minus"
-                    onClick={()=> {console.log('minus clicked')}}
-                    style={{ fontSize: "1.5em", zIndex: 2, marginTop: "3.5px", cursor:"pointer"}}
-                  >
-                    -
-                  </span>
-                  <input
-                    id="product-card-qty-value"
-                    type="number"
-                    className="count"
-                    name="qty"
-                    defaultValue="1"
-                    style={{
-                      width: "4.5em",
-                      textAlign: "center",
-                      zIndex: 1,
-                      marginLeft: "-15px",
-                      marginRight: "-15px",
-                    }}
-                  />
-                  <span
-                    id="product-card-qty-plus"
-                    className="plus"
-                    onClick={()=> {console.log('plus clicked')}}
-                    style={{ fontSize: "1em", zIndex: 2, marginTop: "10px", cursor:"pointer"}}
-                  >
-                    +
-                  </span>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
